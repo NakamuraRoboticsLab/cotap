255bad9a9909f6a634bf764311c26933fb354ef4
hezepy <hezepy896688172@gmail.com>
2025-08-29 21:20:42 +0400

    add upper torq obs

--- humanoidverse/envs/decoupled_locomotion/decoupled_with_facet.py
+++ humanoidverse/envs/decoupled_locomotion/decoupled_with_facet.py
@@ -101,6 +101,7 @@ class LeggedRobotDecoupledLocomotionWithFACET(LeggedRobotDecoupledLocomotionStan
         self.max_acc_xy = self.config.facet_params.max_acc_xy
         self.max_vel_xy = self.config.facet_params.max_vel_xy
         self.ee_kp_range = self.config.facet_params.ee_kp_range
+        self.upper_torques = torch.zeros(self.num_envs, self.config.robot.upper_body_actions_dim, device=self.device)
 
         # 代理目标时间步 (Surrogate target time steps)
         # self.surr_steps = [16, 24, 32]  # 可配置的多时间步 (Configurable)
@@ -197,6 +198,8 @@ class LeggedRobotDecoupledLocomotionWithFACET(LeggedRobotDecoupledLocomotionStan
 
         # 执行父类步进 (Execute parent class step)
         result = super().step(actor_state)
+
+        # print("upper_torques:", self.upper_torques)
 
         return result
 
@@ -725,6 +728,40 @@ class LeggedRobotDecoupledLocomotionWithFACET(LeggedRobotDecoupledLocomotionStan
     # def set_is_evaluating(self, command=None):
     #     super().set_is_evaluating()
 
+    def _compute_torques(self, actions):
+        """ Compute torques from actions.
+            Actions can be interpreted as position or velocity targets given to a PD controller, or directly as scaled torques.
+            [NOTE]: torques must have the same dimension as the number of DOFs, even if some DOFs are not actuated.
+        Args:
+            actions (torch.Tensor): Actions
+
+        Returns:
+            [torch.Tensor]: Torques sent to the simulation
+        """
+        actions_scaled = actions * self.config.robot.control.action_scale
+        ref_actions_scaled = actions_scaled.clone()
+        if self.residual_upper_body_action:
+            actions_scaled[:, self.upper_dof_indices] += (self.ref_upper_dof_pos - self.default_dof_pos[:, self.upper_dof_indices])
+        control_type = self.config.robot.control.control_type
+        if control_type=="P":
+            torques = self._kp_scale * self.p_gains*(actions_scaled + self.default_dof_pos - self.simulator.dof_pos) - self._kd_scale * self.d_gains*self.simulator.dof_vel
+            ref_actions_scaled[:, self.upper_dof_indices] = self.ref_upper_dof_pos
+            self.ref_pd_torques = self._kp_scale * self.p_gains*(ref_actions_scaled + self.default_dof_pos - self.simulator.dof_pos) - self._kd_scale * self.d_gains*self.simulator.dof_vel
+            self.upper_torques = torques[:, self.upper_dof_indices] 
+        elif control_type=="V":
+            torques = self._kp_scale * self.p_gains*(actions_scaled - self.simulator.dof_vel) - self._kd_scale * self.d_gains*(self.simulator.dof_vel - self.last_dof_vel)/self.sim_params.dt
+        elif control_type=="T":
+            torques = actions_scaled
+        else:
+            raise NameError(f"Unknown controller type: {control_type}")
+        if self.config.domain_rand.randomize_torque_rfi:
+            torques = torques + (torch.rand_like(torques)*2.-1.) * self.config.domain_rand.rfi_lim * self._rfi_lim_scale * self.torque_limits
+        
+        if self.config.robot.control.clip_torques:
+            return torch.clip(torques, -self.torque_limits, self.torque_limits)
+        else:
+            return torques
+
     ########################### FACET REWARDS ###########################
 
     def _reward_impedance_pos_tracking(self):
@@ -1148,5 +1185,8 @@ class LeggedRobotDecoupledLocomotionWithFACET(LeggedRobotDecoupledLocomotionStan
                                 )
 
     # ============= Observations =============
+
+    def _get_obs_upper_torq(self):
+        return self.upper_torques
 
     
